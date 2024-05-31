d49eac075b60648a2d60fd914ed1210fc0cfb73c
@ManZhou <2830634496@qq.com>
2024-05-31 17:13:01 +0800

    1.更新django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 添加项目进去
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
