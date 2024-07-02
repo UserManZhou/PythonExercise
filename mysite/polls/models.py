7dcdfb7849e6ca029826a302a5c1f39b97e27bb8
@ManZhou <2830634496@qq.com>
2024-07-02 08:59:17 +0800

    1.修改model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
-
+# 创建问题模型
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    # 添加问题主键
+    question_id = models.BigAutoField(primary_key=True, db_comment="问题主键")
+    # 添加问题内容
+    question_text = models.CharField(max_length=200, db_comment="问题")
+    # 添加问题发布时间
+    pub_date = models.DateTimeField("date published", db_comment="发布时间")
 
 
+# 创建选项模型
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    # 添加选项主键
+    choice_id = models.BigAutoField(primary_key=True, db_comment="选项主键")
+    # 添加选项关联问题
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, db_comment="问题关联")
+    # 添加选项内容
+    choice_text = models.CharField(max_length=200, db_comment="选项")
+    # 添加选项票数
+    votes = models.IntegerField(default=0, db_comment="投票数")
